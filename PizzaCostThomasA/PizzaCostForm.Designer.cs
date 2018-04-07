72980dca4c8e89bc4714174fb5f79b056a306be2
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-04-07 16:39:16 -0400

    Final With Screenshots

--- PizzaCostThomasA/PizzaCostForm.Designer.cs
+++ PizzaCostThomasA/PizzaCostForm.Designer.cs
@@ -28,20 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblDiameterPrompt = new System.Windows.Forms.Label();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblTotalPrompt = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblDiameterPrompt
+            // 
+            this.lblDiameterPrompt.AutoSize = true;
+            this.lblDiameterPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiameterPrompt.Location = new System.Drawing.Point(22, 52);
+            this.lblDiameterPrompt.Name = "lblDiameterPrompt";
+            this.lblDiameterPrompt.Size = new System.Drawing.Size(361, 24);
+            this.lblDiameterPrompt.TabIndex = 0;
+            this.lblDiameterPrompt.Text = "Enter the diameter of the pizza (in inches):";
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.Location = new System.Drawing.Point(413, 57);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(100, 20);
+            this.txtDiameter.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(39, 128);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(98, 40);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblTotalPrompt
+            // 
+            this.lblTotalPrompt.AutoSize = true;
+            this.lblTotalPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPrompt.Location = new System.Drawing.Point(35, 216);
+            this.lblTotalPrompt.Name = "lblTotalPrompt";
+            this.lblTotalPrompt.Size = new System.Drawing.Size(229, 20);
+            this.lblTotalPrompt.TabIndex = 3;
+            this.lblTotalPrompt.Text = "The total cost (including tax) is: ";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(270, 216);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 20);
+            this.lblTotal.TabIndex = 4;
             // 
             // frmPizzaCost
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(619, 282);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblTotalPrompt);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.lblDiameterPrompt);
             this.Name = "frmPizzaCost";
             this.Text = "Pizza Cost By Thomas Aubin";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDiameterPrompt;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblTotalPrompt;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
--- PizzaCostThomasA/PizzaCostForm.cs
+++ PizzaCostThomasA/PizzaCostForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Thomas Aubin
+ * Created on: 7 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #11 - Pseudocode
+ * This program calculates the cost of pizza regarding it's diameter in inches
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +20,39 @@ namespace PizzaCostThomasA
 {
     public partial class frmPizzaCost : Form
     {
+
+        // Create variables
+        const double HST = 0.13;
+        const double LABOUR = 0.75;
+        const double RENT = 0.99;
+        const double MATERIALS = 0.5;
+        double diameter, subtotal, hstCost, total;
+
         public frmPizzaCost()
         {
             InitializeComponent();
+
+            // Hide total cost at beginning
+            lblTotalPrompt.Hide();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Get diameter of pizza
+            diameter = Convert.ToInt32(txtDiameter.Text);
+
+            // Calculate subtotal
+            subtotal = LABOUR + RENT + (diameter * MATERIALS);
+
+            // Calculate hst
+            hstCost = subtotal * HST;
+
+            // Calcualte total
+            total = subtotal + hstCost;
+
+            // Display total and total prompt
+            lblTotal.Text = String.Format("${0:0.00}", total);
+            lblTotalPrompt.Show();
         }
     }
 }
